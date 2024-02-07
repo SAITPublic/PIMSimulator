05b738ef4dc25f8277df4a2f7279df23ec32b329
Sanghoon Cha <s.h.cha@samsung.com>
2024-02-07 16:24:06 +0900

    Deleted dummy code in PIMCRFcode(PIMCmdGen)
    Signed-off-by: Sanghoon Cha <s.h.cha@samsung.com>

--- src/tests/PIMCmdGen.h
+++ src/tests/PIMCmdGen.h
@@ -35,40 +35,6 @@ class IPIMCmd
     KernelType kernelType;
 };
 
-/*
-class BatchNormPIMKernel : public IPIMCmd
-{
-  public:
-    BatchNormPIMKernel(KernelType ktype) : IPIMCmd(ktype) {}
-    virtual vector<PIMCmd> generateKernel(int num_jump_to_be_taken,
-                                          int num_jump_to_be_taken_odd_bank = 0,
-                                          int num_jump_to_be_taken_even_bank = 0) override
-    {
-        vector<PIMCmd> pim_cmds;
-        vector<PIMCmd> tmp_cmds
-        {
-            PIMCmd(PIMCmdType::MAD, PIMOpdType::GRF_A, PIMOpdType::EVEN_BANK, PIMOpdType::SRF_M,
-                   PIMOpdType::SRF_A, 1, 0, 0, 0),
-            PIMCmd(PIMCmdType::MAD, PIMOpdType::GRF_A, PIMOpdType::GRF_A, PIMOpdType::SRF_M,
-                   PIMOpdType::SRF_A, 1, 0, 0, 1),
-            PIMCmd(PIMCmdType::NOP, 7),
-            PIMCmd(PIMCmdType::MAD, PIMOpdType::GRF_B, PIMOpdType::ODD_BANK, PIMOpdType::SRF_M,
-                   PIMOpdType::SRF_A, 1, 0, 0, 0),
-            PIMCmd(PIMCmdType::MAD, PIMOpdType::GRF_B, PIMOpdType::GRF_B, PIMOpdType::SRF_M,
-                   PIMOpdType::SRF_A, 1, 0, 0, 1),
-            PIMCmd(PIMCmdType::NOP, 7),
-            PIMCmd(PIMCmdType::NOP, 0)
-        };
-        pim_cmds.assign(tmp_cmds.begin(), tmp_cmds.end());
-        if (num_jump_to_be_taken != 0)
-        {
-            pim_cmds.push_back(PIMCmd(PIMCmdType::JUMP, num_jump_to_be_taken, pim_cmds.size() + 1));
-        }
-        pim_cmds.push_back(PIMCmd(PIMCmdType::EXIT, 0));
-        return pim_cmds;
-    }
-};
-*/
 class EltwisePIMKernel : public IPIMCmd
 {
   public:
@@ -85,9 +51,7 @@ class EltwisePIMKernel : public IPIMCmd
             PIMCmd(PIMCmdType::NOP, 7),
             PIMCmd(PIMCmdType::FILL, PIMOpdType::GRF_B, PIMOpdType::ODD_BANK),
             PIMCmd(pimType, PIMOpdType::GRF_B, PIMOpdType::GRF_B, PIMOpdType::ODD_BANK, 1),
-            PIMCmd(PIMCmdType::NOP, 7),
-            PIMCmd(PIMCmdType::NOP, 0),
-        };
+            PIMCmd(PIMCmdType::NOP, 7)};
         pim_cmds.assign(tmp_cmds.begin(), tmp_cmds.end());
         if (num_jump_to_be_taken != 0)
         {
@@ -124,7 +88,7 @@ class ActPIMKernel : public IPIMCmd
                 PIMCmd(PIMCmdType::FILL, PIMOpdType::GRF_A, PIMOpdType::EVEN_BANK, 1, 0, 0, 0, 1),
                 PIMCmd(PIMCmdType::NOP, 7),
                 PIMCmd(PIMCmdType::FILL, PIMOpdType::GRF_B, PIMOpdType::ODD_BANK, 1, 0, 0, 0, 1),
-                PIMCmd(PIMCmdType::NOP, 7), PIMCmd(PIMCmdType::NOP, 0)};
+                PIMCmd(PIMCmdType::NOP, 7)};
             pim_cmds.assign(tmp_cmds.begin(), tmp_cmds.end());
         }
         else
